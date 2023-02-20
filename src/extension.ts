78263dcd1edd8857d258eda58999879897fac177
2043869032@qq.com <2043869032@qq.com>
2023-02-20 14:45:19 +0800

    chore: 修改架构

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { ExtensionContext } from 'vscode';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { ElementHoverProvier } from './hover-tips/element-hover-provider';
+import { ElementCompletionItemProvider } from './completion/element-completion-item-povider';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "element-plus-snippets" is now active!');
+export function activate(context: ExtensionContext): void {
+  console.log('extension "element-ui-helper" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('element-plus-snippets.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from element-plus-snippets!');
-	});
+  // 注册 completion 提示
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      [
+        {
+          language: 'vue',
+          scheme: 'file'
+        }
+      ],
+      new ElementCompletionItemProvider(),
+      '',
+      ' ',
+      ':',
+      '<',
+      '"',
+      "'",
+      '/',
+      '@',
+      '(',
+      '-'
+    )
+  );
 
-	context.subscriptions.push(disposable);
+  // 注册 hover 提示
+  context.subscriptions.push(
+    vscode.languages.registerHoverProvider(
+      [
+        {
+          language: 'vue',
+          scheme: 'file'
+        }
+      ],
+      new ElementHoverProvier()
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
+// this method is called when your extension is deactivated
 export function deactivate() {}
+
+export default {
+  activate,
+  deactivate
+};
